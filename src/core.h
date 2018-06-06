006f091b725e6bacd0f65880e441961963ca2edf
柴宇进 <chaiyujin@chaiyujins-MacBook-Pro.local>
2018-06-07 00:29:56 +0800

    ffmpeg

--- src/core.h
+++ src/core.h
@@ -1,40 +1,98 @@
 #pragma once
 
-#define NAMESPACE_BEGIN(name) namespace name {
-#define NAMESPACE_END(name) }
-
-
-
-#ifndef __YUKI_UTILS_LOG_H__
-#define __YUKI_UTILS_LOG_H__
-
-// #define CHECK_LEVEL_NONE // uncomment this line to disable runtime check
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdarg.h>
 
-#define YUKI_LOG(fmt, ...) { printf("[LOG] "); printf(fmt, ##__VA_ARGS__); }
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <queue>
 
-#ifdef NDEBUG
-#define YUKI_DEBUG(fmt, ...) {}
-#else
-#define YUKI_DEBUG(fmt, ...) { printf("[DEBUG] "); printf(fmt, ##__VA_ARGS__); }
-#endif
+namespace yuki
+{
+	#ifndef __YUKI_UTILS_LOG_H__
+	#define __YUKI_UTILS_LOG_H__
 
-#define YUKI_ERROR(fmt, ...) {\
-	printf("Error in [%s] (line: %d) : ", __FUNCTION__, __LINE__);\
-	printf(fmt, ##__VA_ARGS__); }
-#define YUKI_ERROR_EXIT(fmt, ...) {\
-	printf("Error in [%s] (line: %d) : ", __FUNCTION__, __LINE__);\
-	printf(fmt, ##__VA_ARGS__);\
-	getchar(); exit(1); }
-#define CHECK(assertion) {\
-		bool flag = assertion;\
-		if (!flag) {\
-			printf("Error in [%s] (line: %d) : CHECK fail.\n", __FUNCTION__, __LINE__);\
-			getchar(); exit(1);\
-		}\
-	}
+	// #define CHECK_LEVEL_NONE // uncomment this line to disable runtime check
 
-#endif // !__YUKI_UTILS_LOG_H__
+
+	#define YUKI_LOG(fmt, ...) { printf("[LOG] "); printf(fmt, ##__VA_ARGS__); }
+
+	#ifdef NDEBUG
+	#define YUKI_DEBUG(fmt, ...) {}
+	#else
+	#define YUKI_DEBUG(fmt, ...) { printf("[DEBUG] "); printf(fmt, ##__VA_ARGS__); }
+	#endif
+
+	#define YUKI_ERROR(fmt, ...) {\
+		printf("Error in [%s] (line: %d) : ", __FUNCTION__, __LINE__);\
+		printf(fmt, ##__VA_ARGS__); }
+	#define YUKI_ERROR_EXIT(fmt, ...) {\
+		printf("Error in [%s] (line: %d) : ", __FUNCTION__, __LINE__);\
+		printf(fmt, ##__VA_ARGS__);\
+		getchar(); exit(1); }
+	#define CHECK(assertion) {\
+			bool flag = assertion;\
+			if (!flag) {\
+				printf("Error in [%s] (line: %d) : CHECK fail.\n", __FUNCTION__, __LINE__);\
+				getchar(); exit(1);\
+			}\
+		}
+
+	#endif // !__YUKI_UTILS_LOG_H__
+}
+
+namespace yuki
+{
+	template <class T>
+	class SafeQueue
+	{
+	public:
+		SafeQueue(void): q_(), m_(), c_(), size_(0) {}
+		~SafeQueue(void){}
+		// Add an element to the queue.
+		void push(T t)
+		{
+			std::lock_guard<std::mutex> lock(m_);
+			q_.push(t);
+			size_++;
+			c_.notify_one();
+		}
+		// Get the "front"-element.
+		// If the queue is empty, wait till a element is avaiable.
+		void pop(void)
+		{
+			std::unique_lock<std::mutex> lock(m_);
+			while (q_.empty())
+			{
+				// release lock as long as the wait and reaquire it afterwards.
+				c_.wait(lock);
+			}
+			q_.pop();
+			size_--;
+		}
+		T &front()
+		{
+			std::unique_lock<std::mutex> lock(m_);
+			while (q_.empty())
+			{
+				// release lock as long as the wait and reaquire it afterwards.
+				c_.wait(lock);
+			}
+			return q_.front();
+		}
+
+		size_t size() const { return size_; }
+
+	private:
+		std::queue<T> q_;
+		mutable std::mutex m_;
+		std::condition_variable c_;
+		size_t size_;
+	};
+}
+
+
+#define NAMESPACE_BEGIN(name) namespace name {
+#define NAMESPACE_END(name) }
---
+++ src/media/ffmpeg.h
@@ -0,0 +1,118 @@
+#pragma once
+
+extern "C" {
+	#ifndef __STDC_CONSTANT_MACROS
+	#define __STDC_CONSTANT_MACROS
+	#endif
+
+    #include <libavutil/opt.h>
+    #include <libavutil/dict.h>
+    #include <libavutil/eval.h>
+    #include <libavutil/fifo.h>
+    #include <libavutil/time.h>
+    #include <libavutil/avutil.h>
+    #include <libavutil/pixfmt.h>
+    #include <libavutil/pixdesc.h>
+    #include <libavutil/rational.h>
+    #include <libavutil/timestamp.h>
+    #include <libavutil/samplefmt.h>
+    #include <libavutil/hwcontext.h>
+    #include <libavutil/audio_fifo.h>
+    #include <libavutil/threadmessage.h>
+    #include <libavutil/channel_layout.h>
+    #include <libavformat/avio.h>
+    #include <libavformat/avformat.h>
+    #include <libavcodec/avcodec.h>
+    #include <libavfilter/avfilter.h>
+    #include <libavfilter/buffersink.h>
+    #include <libavfilter/buffersrc.h>
+    #include <libswscale/swscale.h>
+    #include <libswresample/swresample.h>
+    #include <libavdevice/avdevice.h>
+};
+
+#include <memory>
+
+namespace yuki {
+namespace media {
+
+    enum Type
+    {
+        Audio = 0,
+        Video = 1
+    };
+
+    class Frame
+    {
+    protected:
+        std::shared_ptr<uint8_t>    data_;
+        double                      ts_;
+    public:
+        Frame() : data_(NULL), ts_(0) {}
+        virtual ~Frame() {}
+        bool is_null() const { return !data_; }
+        uint8_t *data() { return data_.get(); }
+        const uint8_t *data() const { return data_.get(); }
+    };
+
+    class VideoFrame : public Frame
+	{
+		int			                w_, h_, pixels_;
+		bool		                is_depth_;
+	public:
+        VideoFrame() : Frame(), w_(0), h_(0), pixels_(0), is_depth_(false) {}
+        VideoFrame(AVFrame *frame, double pts, bool depth)
+            : Frame() { from_avframe(frame, pts, depth); }
+
+        int width() const { return w_; }
+        int height() const { return h_; }
+        int pixels() const { return pixels_; }
+        bool is_depth() const { return is_depth_; }
+
+        // only RGBA or DepthRVL
+		void from_avframe(AVFrame *frame, double pts, bool depth)
+		{
+			is_depth_ = depth;
+			int size = frame->width * frame->height * 2;	// uint16_t
+			if (!depth) size *= 2;							// RGBA
+			data_.reset(new uint8_t[size]);
+			memcpy(data_.get(), frame->data[0], size);
+			w_ = frame->width;
+			h_ = frame->height;
+            pixels_ = w_ * h_;
+			ts_ = pts;
+		}
+	};
+
+	struct AudioFrame : public Frame
+	{
+		int			                nb_samples_;
+		int			                byte_per_sample_;
+    public:
+        AudioFrame() : Frame(), nb_samples_(0), byte_per_sample_(0) {}
+        AudioFrame(AVFrame *frame, double pts, int nb_samples, int byte_per_sample)
+            : Frame() { from_avframe(frame, pts, nb_samples, byte_per_sample); }
+
+        int nb_samples() const { return nb_samples_; }
+        int byte_per_sample() const { return byte_per_sample_; }
+
+		void from_avframe(AVFrame *frame, double pts, int nb_samples, int byte_per_sample)
+		{
+			nb_samples_ = nb_samples;
+			byte_per_sample_ = byte_per_sample;
+			data_.reset(new uint8_t[nb_samples_ * byte_per_sample_]);
+			memcpy(data_.get(), frame->data[0], nb_samples_ * byte_per_sample_);
+			ts_ = pts;
+		}
+	};
+
+    template <class T> class Stream;
+    template <>
+    class Stream<AudioFrame>
+    {
+    public:
+        const media::Type Type = media::Type::Audio;
+        
+    };
+
+}}
